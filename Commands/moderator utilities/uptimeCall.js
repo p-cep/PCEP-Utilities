fb8c12a5ec9d74e5fdba85895a5cfd3f5187745e
Mikecerc1 <mikecerc1@outlook.com>
2021-12-15 17:21:56 -0500

    fix to uptime

--- Commands/moderator utilities/uptimeCall.js
+++ Commands/moderator utilities/uptimeCall.js
@@ -4,14 +4,16 @@ module.exports = {
   name: "uptime",
   description: "Display's the amount of time P-CEP Utilities has been online",
   execute(interaction) {
-    let uptimeSeconds = process.uptime();
-    let hours = Math.floor(uptimeSeconds / (60 * 60));
-    let minutes = Math.floor((uptimeSeconds % (60 * 60)) / 60);
-    let seconds = Math.floor(uptimeSeconds % 60);
-    let day = Math.floor(hours / 24);
-    if (hours >= 24) {
-      hours = hours - 24;
-    }
+    let seconds = process.uptime();
+    var d = Math.floor(seconds / (3600*24));
+    var h = Math.floor(seconds % (3600*24) / 3600);
+    var m = Math.floor(seconds % 3600 / 60);
+    var s = Math.floor(seconds % 60);
+
+    var day = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+    var hours = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var minutes = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    var seconds = s > 0 ? s + (s == 1 ? " second" : " seconds") : ""; 
 
     let embed = new MessageEmbed()
     .setColor('RANDOM')
